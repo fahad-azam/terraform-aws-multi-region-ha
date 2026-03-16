ed43461e7e87fd65d67d047ac7554cfe54709d99
fahad-azam <azamfahad611@gmail.com>
2026-03-16 10:46:50 +0530

    minor-fix

--- database-layer/main.tf
+++ database-layer/main.tf
@@ -4,7 +4,7 @@ module "database" {
   project_name        = var.project_name
   primary_region_aws  = var.primary_region_aws
   standby_region_aws  = var.standby_region_aws
-  environment         = var.environment
+  environment         = local.environment
   db_params           = var.db_params
 
   providers = {
